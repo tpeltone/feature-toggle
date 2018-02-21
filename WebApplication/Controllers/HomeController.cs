836d28891efadba83f5b96e6cd6be74b88973421
Tomi Peltonen <tomi.peltonen@outlook.com>
2018-02-21 21:37:26 +0100

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }       
+
+        public ActionResult Page1()
+        {
+            ViewBag.IsImported = bool.Parse(ConfigurationManager.AppSettings["ImportFeature"]);
+
+            return View();
+        }
+
+        public ActionResult Page2()
+        {
+            ViewBag.IsImported = bool.Parse(ConfigurationManager.AppSettings["ImportFeature"]);
+
+            return View();
+        }
+    }
+}
